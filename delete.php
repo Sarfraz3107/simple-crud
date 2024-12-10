7eeb1f28bfa5fbc385c876b2f4deb064c90fa641
Sarfraz3107 <mdsarfraz2288@gmail.com>
2024-12-10 17:41:21 +0530

    crud operation

---
+++ delete.php
@@ -0,0 +1,15 @@
+
+<?php
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    include('connect.php');
+    $sql = "DELETE FROM books where id=$id";
+    if(mysqli_query($conn , $sql)){
+        session_start();
+        $_SESSION["delete"] = "Book Deleted Successfully"; 
+       header("Location:index.php");
+    }
+}
+
+?>
